08e328805eb1874ea6f13a970c317559d6e55e62
Ren-Oliveira <ren.jun.oliveira@gmail.com>
2022-02-07 11:52:46 +0000

    finished random quotes maching 12/12 --- screw class base react!

--- freeCodeCamp/Random_quotes/random_quotes.js
+++ freeCodeCamp/Random_quotes/random_quotes.js
@@ -1,21 +1,55 @@
-class MyComponent extends React.Component {
-  quote = "a bcd";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+import { useState, useEffect } from "https://cdn.skypack.dev/react";
 
-  render() {
-    return (
-      <div id="wrapper">
-        <div id="quote-box">
-          <div id="text"> {this.quote} </div>
-          <div id="author" />
-          <button id="new-quote"> New Quote </button>
+const QuoteMachine = () => {
+  const [quote, setQuote] = useState({ autor: null, text: null });
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSearching, setIsSearching] = useState(false);
 
-          <a id="tweet-quote" target="_top" href="twitter.com/intent/tweet">
+  useEffect(async () => {
+    setIsLoading(true);
+    const response = await fetch("https://api.quotable.io/random");
+    if (response.ok) {
+      const data = await response.json();
+      setQuote({ autor: data.author, text: data.content });
+      setIsLoading(false);
+    } else {
+      setQuote({
+        autor: "Admin",
+        text: "An error has occurred, please try again!",
+      });
+      setIsLoading(false);
+    }
+    setIsSearching(false);
+  }, [isSearching]);
+
+  const next = () => {
+    return setIsSearching(true);
+  };
+
+  return (
+    <div id="wrapper">
+      <div id="quote-box">
+        {isLoading && <div className="loading" />}
+        {!isLoading && (
+          <>
+            <div id="text">{quote.text}</div>
+            <div id="author">{quote.author}</div>
+          </>
+        )}
+
+        <div className="btns">
+          <button type="button" id="new-quote" onClick={next}>
+            {" "}
+            Next Quote{" "}
+          </button>
+          <a id="tweet-quote" target="_blank" href="twitter.com/intent/tweet">
             <button>Tweet</button>
           </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-ReactDOM.render(<MyComponent />, document.querySelector("#app"));
+ReactDOM.render(<QuoteMachine />, document.getElementById("root"));
