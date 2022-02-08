dbf9d8d058c03b3438a679ff1c41f0bee35d2583
Ren-Oliveira <ren.jun.oliveira@gmail.com>
2022-02-08 13:55:53 +0000

    Updated Markdown.js (5/8)

--- content/freeCodeCamp/Markdown_previewer/Markdown.js
+++ content/freeCodeCamp/Markdown_previewer/Markdown.js
@@ -1,14 +1,32 @@
 import ReactDOM from "https://cdn.skypack.dev/react-dom";
+import { useState } from "https://cdn.skypack.dev/react";
+import { marked } from "https://cdn.skypack.dev/marked";
+
 
 const MarkdownPreviewer = () => {
+  const [input, setInput] = useState("");
+  
+  const changeHandler = (e) => {
+    return setInput(e.target.value)
+  }
+  
+const markdown = marked(input);
+  
   return (
     <div>
-      <div className="editor-box">
-        <textarea id="editor" />
+      <div className="box">
+        <h4 className="title">Editor</h4>
+        <textarea id="editor" value={input} onChange={changeHandler}/>
       </div>
-      <div id="preview" />
+      
+      <div className="box">
+        <h4 className="title">Preview</h4>
+      <div id="preview" dangerouslySetInnerHTML={{__html: markdown}}></div>
+      </div>
+      
     </div>
-  );
+  )
 };
+
 
 ReactDOM.render(<MarkdownPreviewer />, document.getElementById("root"));
