3dd5975bb1edd2e8e060d4f1aab14e03731fcd09
Ren-Oliveira <ren.jun.oliveira@gmail.com>
2022-02-12 13:04:42 +0000

    Updated fcc_clock sass and js - everything ready to start logic

--- content/freeCodeCamp/Frontend/25+5_clock/clock.js
+++ content/freeCodeCamp/Frontend/25+5_clock/clock.js
@@ -1,26 +1,31 @@
 import ReactDOM from "https://cdn.skypack.dev/react-dom";
+import { useState } from "https://cdn.skypack.dev/react";
 
 const Clock = () => {
+  const [min, setMin] = useState(25);
+  const [sec, setSec] = useState(0);
+  
+  
   return(
     <div className="container">
 
             <div id="timer">
         <div id="timer-label"> Session </div>
-        <div id="time-left"> 25:00 </div>
+        <div id="time-left"> {min}:{sec} </div>
       </div>  
       
-      <div id="session">
-        <div id="session-label"> Session length </div>
-        <button id="session-decrement"> - </button>  
-        <span className="n-label"> 5 </span>
-        <button id="session-increment"> + </button>
+      <div id="session" className="box">
+        <div id="session-label" className="label"> Session Length </div>
+        <button id="session-decrement" className="btn"> - </button>
+        <span className="value"> {min} </span>
+        <button id="session-increment" className="btn"> + </button>
       </div>  
 
-      <div id="break">
-        <div id="break-label"> Break Length </div>
-        <button id="break-decrement"> - </button> 
-        <span className="n-label"> 5 </span> 
-        <button id="break-increment"> + </button>  
+      <div id="break" className="box">
+        <div id="break-label" className="label"> Break Length </div>
+        <button id="break-decrement" className="btn"> - </button> 
+        <span className="value"> 5 </span>
+        <button id="break-increment" className="btn"> + </button>  
       </div>
 
       <div className="btns">
