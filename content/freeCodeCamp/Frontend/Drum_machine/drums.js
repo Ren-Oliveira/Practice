7da0724acdcbb6ad1b645165fecef4ea2758ed69
Ren-Oliveira <ren.jun.oliveira@gmail.com>
2022-02-17 19:51:35 +0000

    DRY! removed repetead code!

--- content/freeCodeCamp/Frontend/Drum_machine/drums.js
+++ content/freeCodeCamp/Frontend/Drum_machine/drums.js
@@ -56,47 +56,18 @@ const bankOne = [
 ];
 
 const Drums = () => {
+  const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+
   return (
     <div id="drum-machine">
       <div id="container">
         <div id="left-side">
           <div id="drum-pad">
-            <button id="q" className="drum-pad">
-              {" "}
-              Q{" "}
-            </button>
-            <button id="w" className="drum-pad">
-              {" "}
-              W{" "}
-            </button>
-            <button id="e" className="drum-pad">
-              {" "}
-              E{" "}
-            </button>
-            <button id="a" className="drum-pad">
-              {" "}
-              A{" "}
-            </button>
-            <button id="s" className="drum-pad">
-              {" "}
-              S{" "}
-            </button>
-            <button id="d" className="drum-pad">
-              {" "}
-              D{" "}
-            </button>
-            <button id="z" className="drum-pad">
-              {" "}
-              Z{" "}
-            </button>
-            <button id="x" className="drum-pad">
-              {" "}
-              X{" "}
-            </button>
-            <button id="c" className="drum-pad">
-              {" "}
-              C{" "}
-            </button>
+            {keys.map((key) => (
+              <button id={key} className="drum-pad">
+                {key}
+              </button>
+            ))}
           </div>
         </div>
 
