b5bd75bcccea11e322634a6a8432e7f8164959b9
Ren-Oliveira <ren.jun.oliveira@gmail.com>
2022-02-21 22:06:18 +0000

    vanilla revision... detour from d3

--- content/D3/app.js
+++ content/D3/app.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const form = document.getElementById("addItem");
+const inputNum = document.getElementById("inputNum");
+const submitBtn = document.getElementById("submitBtn");
+const display = document.querySelector(".display");
+
+let dataSet = [];
+
+const submitHandler = (e) => {
+  e.preventDefault();
+  if (!inputNum.value || inputNum.value === "") return;
+  dataSet.push(inputNum.value);
+  inputNum.value = null;
+  display.innerHTML = dataSet.join(" ");
+};
+
+submitBtn.addEventListener("click", submitHandler);
+
+// d3.select("div")
+//   .select("#foo")
+//   .data([1, 2, 3])
+//   .enter()
+//   .append("h2")
+//   .text((d) => `${d}€`);
