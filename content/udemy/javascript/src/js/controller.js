a416e3ae3de73aaa2a37b79bb6d11831c63fbe6f
Ren-Oliveira <ren.jun.oliveira@gmail.com>
2022-02-23 15:42:32 +0000

    fecth

--- content/udemy/javascript/src/js/controller.js
+++ content/udemy/javascript/src/js/controller.js
@@ -9,5 +9,35 @@ const timeout = function (s) {
 };
 
 // https://forkify-api.herokuapp.com/v2
-
+// 17f44c94-af19-405d-9614-d49467d28b93
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = data.data;
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.group(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
